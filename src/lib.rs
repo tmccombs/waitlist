2dbb3d0c899f0e667ba5a1bbddc32652e92db4dd
Thayne McCombs <astrothayne@gmail.com>
2019-12-07 01:59:27 -0700

    Implement Waitlist using a VecDeque

--- src/lib.rs
+++ src/lib.rs
@@ -7,24 +7,21 @@ use core::sync::atomic::{AtomicUsize, Ordering};
 use core::task::{Context, Poll, Waker};
 
 extern crate alloc;
+use alloc::collections::vec_deque::VecDeque;
 use alloc::fmt;
-use alloc::vec::Vec;
 
 use crossbeam_utils::Backoff;
 
-enum Slot {
-    Available { next: usize },
-    Waiting { next: usize, waker: Waker },
-    Notified,
+struct Waiter {
+    key: usize,
+    waker: Waker,
 }
 
 struct Inner {
-    queue: Vec<Slot>,
-    waiting_count: usize,
+    queue: VecDeque<Waiter>,
     notified_count: usize,
-    next_avail: usize,
-    next_waiting: usize,
-    last_waiting: usize,
+    min_key: usize,
+    next_key: usize,
 }
 
 // Set when the queue is locked.
@@ -37,10 +34,6 @@ const WAITING: usize = 1 << 1;
 // yet been removed
 const NOTIFIED: usize = 1 << 2;
 
-// Index for the `next` value of the tail of the
-// waiting queue
-const SENTINEL: usize = usize::max_value();
-
 /**
  */
 pub struct Waitlist {
@@ -50,7 +43,7 @@ pub struct Waitlist {
 
 pub struct WaitRef<'a> {
     waitlist: &'a Waitlist,
-    index: usize,
+    key: usize,
 }
 
 pub struct WaitFuture<'a> {
@@ -69,12 +62,10 @@ impl Waitlist {
         Waitlist {
             flags: AtomicUsize::new(0),
             inner: UnsafeCell::new(Inner {
-                queue: Vec::with_capacity(cap),
-                waiting_count: 0,
+                queue: VecDeque::with_capacity(cap),
                 notified_count: 0,
-                next_avail: 0,
-                next_waiting: SENTINEL,
-                last_waiting: SENTINEL,
+                min_key: 0,
+                next_key: 0,
             }),
         }
     }
@@ -90,10 +81,10 @@ impl Waitlist {
     #[inline]
     pub fn insert(&self, cx: &Context) -> WaitRef<'_> {
         let waker = cx.waker().clone();
-        let idx = self.lock().add_waker(waker);
+        let key = self.lock().insert(waker);
         WaitRef {
             waitlist: self,
-            index: idx,
+            key,
         }
     }
 
@@ -152,23 +143,23 @@ impl fmt::Debug for Waitlist {
 
 impl<'a> WaitRef<'a> {
     #[inline]
-    pub fn update(&self, cx: &Context) {
+    pub fn update(&mut self, cx: &Context) {
         let waker = cx.waker().clone();
-        self.waitlist.lock().update(self.index, waker)
+        self.key = self.waitlist.lock().update(self.key, waker);
     }
 
     /// Remove waker associated with this reference from the
     /// waitlist without triggering another notify.
     #[inline]
     pub fn remove(self) -> bool {
-        let was_notified = self.waitlist.lock().remove(self.index);
+        let was_notified = self.waitlist.lock().remove(self.key);
         mem::forget(self); // forget self, because the default is a cancel
         was_notified
     }
 
     #[inline]
     pub fn cancel(self) -> bool {
-        let did_notify = self.waitlist.lock().cancel(self.index);
+        let did_notify = self.waitlist.lock().cancel(self.key);
         mem::forget(self); // forget so we don't try removing it again
         did_notify
     }
@@ -179,7 +170,7 @@ impl<'a> Drop for WaitRef<'a> {
     fn drop(&mut self) {
         // by default dropping a WaitRef will cancel,
         // triggering another waker if necessary
-        self.waitlist.lock().cancel(self.index);
+        self.waitlist.lock().cancel(self.key);
     }
 }
 
@@ -193,7 +184,7 @@ impl<'a> Future for WaitFuture<'a> {
             }
         } else {
             let waker = cx.waker().clone();
-            self.key = Some(self.waitlist.lock().add_waker(waker));
+            self.key = Some(self.waitlist.lock().insert(waker));
         }
         Poll::Pending
     }
@@ -217,83 +208,39 @@ impl Default for Waitlist {
 }
 
 impl Inner {
-    fn add_waker(&mut self, waker: Waker) -> usize {
-        let key = self.next_avail;
-        let entry = Slot::Waiting {
-            next: SENTINEL,
-            waker,
-        };
-        if key == self.queue.len() {
-            self.queue.push(entry);
-            self.next_avail += 1;
-        } else {
-            self.next_avail = match self.queue.get(key) {
-                Some(&Slot::Available { next }) => next,
-                _ => unreachable!(),
-            };
-            self.queue[key] = entry;
-        }
-        self.queue_slot(key);
+    fn is_in_waiting_range(&self, key: usize) -> bool {
+        // the part after `||` is to deal with if the key wraps around
+        key >= self.min_key || (self.next_key < self.min_key && key < self.next_key)
+    }
+
+    fn insert(&mut self, waker: Waker) -> usize {
+        let key = self.next_key;
+        self.next_key = self.next_key.wrapping_add(1);
+        self.queue.push_back(Waiter { key, waker });
         key
     }
 
-    fn update(&mut self, key: usize, w: Waker) {
-        let was_notified = match self.queue.get_mut(key) {
-            Some(slot @ &mut Slot::Notified) => {
-                *slot = Slot::Waiting {
-                    next: SENTINEL,
-                    waker: w,
-                };
-                true
+    fn update(&mut self, key: usize, waker: Waker) -> usize {
+        if self.is_in_waiting_range(key) {
+            if let Some(w) = self.queue.iter_mut().find(|w| w.key == key) {
+                w.waker = waker;
+                return key;
             }
-            Some(&mut Slot::Waiting { ref mut waker, .. }) => {
-                *waker = w;
-                false
-            }
-            _ => unreachable!(),
-        };
-        if was_notified {
-            // if the slot was already notified, add the slot back to the end of the
-            // wait queue
-            self.queue_slot(key);
-            self.notified_count -= 1;
         }
+        self.notified_count -= 1; // the waiter was already notified, so we need to decrement the number actively notified tasks
+        self.insert(waker)
     }
 
     #[cold]
     fn remove(&mut self, key: usize) -> bool {
-        let entry = self.take_slot(key);
-        match entry {
-            Slot::Waiting {
-                next: next_slot, ..
-            } => {
-                self.waiting_count -= 1;
-                if key == self.next_waiting {
-                    self.next_waiting = next_slot;
-                } else {
-                    let mut current = self.next_waiting;
-                    // find and update the previous entry in the queue
-                    while current != SENTINEL {
-                        if let Slot::Waiting { ref mut next, .. } = self.queue[current] {
-                            if *next == key {
-                                *next = next_slot;
-                                break;
-                            } else {
-                                current = *next;
-                            }
-                        } else {
-                            unreachable!();
-                        }
-                    }
-                }
-                false
+        if self.is_in_waiting_range(key) {
+            if let Some(idx) = self.queue.iter().position(|w| w.key == key) {
+                self.queue.remove(idx);
+                return false;
             }
-            Slot::Notified => {
-                self.notified_count -= 1;
-                true
-            }
-            Slot::Available { .. } => unreachable!("attempt to remove non-existant entry"),
         }
+        self.notified_count -= 1;
+        true
     }
 
     fn cancel(&mut self, key: usize) -> bool {
@@ -305,33 +252,23 @@ impl Inner {
     }
 
     fn remove_if_notified(&mut self, key: usize, cx: &Context<'_>) -> bool {
-        let should_remove = match self.queue[key] {
-            Slot::Waiting { ref mut waker, .. } => {
-                *waker = cx.waker().clone();
-                false
+        // all we really need to do here is decrement notified_count if the key isn't in the queue
+        if self.is_in_waiting_range(key) {
+            if let Some(w) = self.queue.iter_mut().find(|w| w.key == key) {
+                w.waker = cx.waker().clone();
+                return false;
             }
-            Slot::Notified => true,
-            Slot::Available { .. } => unreachable!("Attempt to remove non-existant entry"),
-        };
-        if should_remove {
-            self.queue[key] = Slot::Available {
-                next: self.next_avail,
-            };
-            self.next_avail = key;
-            self.notified_count -= 1;
         }
-        should_remove
+        self.notified_count -= 1;
+        true
     }
 
     fn notify_first(&mut self) -> bool {
-        let key = self.next_waiting;
-        if key != SENTINEL {
-            self.next_waiting = self.notify_slot(key);
-            self.waiting_count -= 1;
+        if let Some(waiter) = self.queue.pop_front() {
             self.notified_count += 1;
-            if self.next_waiting == SENTINEL {
-                self.last_waiting = SENTINEL;
-            }
+            debug_assert!(waiter.key >= self.min_key);
+            self.min_key = waiter.key.wrapping_add(1);
+            waiter.waker.wake();
             true
         } else {
             false
@@ -339,50 +276,13 @@ impl Inner {
     }
 
     fn notify_all(&mut self) -> bool {
-        let notified = self.next_waiting != SENTINEL;
-        let mut current = self.next_waiting;
-        while current != SENTINEL {
-            current = self.notify_slot(current);
+        let num_notified = self.queue.len();
+        while let Some(w) = self.queue.pop_front() {
+            w.waker.wake();
         }
-        self.notified_count += self.waiting_count;
-        self.waiting_count = 0;
-        self.next_waiting = SENTINEL;
-        self.last_waiting = SENTINEL;
-        notified
-    }
-
-    fn notify_slot(&mut self, key: usize) -> usize {
-        if let Slot::Waiting { next, waker } = mem::replace(&mut self.queue[key], Slot::Notified) {
-            waker.wake();
-            next
-        } else {
-            unreachable!()
-        }
-    }
-
-    fn take_slot(&mut self, key: usize) -> Slot {
-        let slot = mem::replace(
-            &mut self.queue[key],
-            Slot::Available {
-                next: self.next_avail,
-            },
-        );
-        self.next_avail = key;
-        slot
-    }
-
-    fn queue_slot(&mut self, key: usize) {
-        if self.last_waiting == SENTINEL {
-            // This is the first waiter, so update both ends of the queue
-            self.next_waiting = key;
-            self.last_waiting = key;
-        } else if let Slot::Waiting { ref mut next, .. } = self.queue[self.last_waiting] {
-            *next = key;
-            self.last_waiting = key;
-        } else {
-            unreachable!();
-        }
-        self.waiting_count += 1;
+        self.notified_count += num_notified;
+        self.min_key = self.next_key;
+        num_notified > 0
     }
 }
 
@@ -410,7 +310,7 @@ impl<'a> Drop for Guard<'a> {
     fn drop(&mut self) {
         let mut flags = 0;
 
-        if self.waiting_count > 0 {
+        if !self.queue.is_empty() {
             flags |= WAITING;
         }
 
